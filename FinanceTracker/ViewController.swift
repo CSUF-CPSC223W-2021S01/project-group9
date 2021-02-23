e15a97d8b4ccfeaab3f0932ef682ff655760f01a
Arm627 <monted97@gmail.com>
2021-02-23 10:38:36 -0800

    Initial Commit

---
+++ FinanceTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceTracker
+//
+//  Created by Monte  Davityan  on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
