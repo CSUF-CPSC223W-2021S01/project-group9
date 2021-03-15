e746232a8d2f618b0135ba3883d1fb751a0ac350
Arm627 <monted97@gmail.com>
2021-03-15 15:29:24 -0700

    Finished post request to get transaction data

--- FinanceTracker/Transactions.swift
+++ FinanceTracker/Transactions.swift
@@ -16,10 +16,42 @@ class getTransactions {
     let clientId = "602e9d8caaddb00010348259"
     let secret = "d4bcd044d1751fba527799f724b043"
     
+    //TODO: make these dynamic to the device (ie, store this id)
     let tempAccessToken = "access-sandbox-19c042cd-833b-4d52-a962-d5bcf0044a48"
     
-    let startDate = "2020-10-10"
-    let endDate = "2021-03-01"
+    let startDate = "2021-03-1"
+    let endDate = "2021-03-03"
     
-    //TODO: POST request to get transactions data (see curl request above)
+    func requestData(completion: @escaping ([String: Any]) -> ()) {
+        
+        let url = URL(string: self.transactionUrl)
+        let payload = "{\"client_id\":\"\(self.clientId)\",\"secret\":\"\(self.secret)\",\"access_token\":\"\(self.tempAccessToken)\", \"start_date\":\"2018-01-01\", \"end_date\":\"2021-03-01\"}".data(using: .utf8)
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = payload
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+
+            guard let data = data else {
+                print("error")
+                return
+            }
+            if let str = String(data: data, encoding: .utf8) {
+                return completion(self.convertToDictionary(text: str) ?? [String: Any]())
+            }
+        }.resume()
+    }
+    
+    func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
 }
