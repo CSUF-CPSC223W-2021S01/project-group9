f8d979cda38ba69b20105fdb3b1b3c69a824a8dc
sbhagat101 <54718068+sbhagat101@users.noreply.github.com>
2021-03-23 20:00:48 -0700

    add test graphs

--- FinanceTracker/GraphController.swift
+++ FinanceTracker/GraphController.swift
@@ -3,15 +3,29 @@
 //  FinanceTracker
 //
 //  Created by Justin Lee on 3/22/21.
-//
+//  Modifed by Shiv Bhagat on 3/23/2021
 
 import UIKit
+import SwiftUICharts
+import SwiftUI
 
-class GraphController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+struct GraphController: View {
+    var body: some View{
+        VStack{
+            Spacer()
+            // Line chart
+            LineChartView(data: [12,22,6,1,2,7,18], title: "Test LineChart")
+            Spacer()
+             // Bar Chart
+            BarChartView(data: ChartData(values:[("Jan", 12) , ("Feb", 2) , ("Mar", 3)]
+            ) , title: "Test Bar Graph")
+        }
     }
-    
 }
+
+struct GraphController_previews: PreviewProvider {
+    static var previews: some View {
+        GraphController()
+    }
+}
+
