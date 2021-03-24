39586ed8af0aba0090b42f321ea58eecdb5659f5
sbhagat101 <54718068+sbhagat101@users.noreply.github.com>
2021-03-24 16:05:41 -0700

    k

--- FinanceTracker/GraphController.swift
+++ FinanceTracker/GraphController.swift
@@ -3,29 +3,14 @@
 //  FinanceTracker
 //
 //  Created by Justin Lee on 3/22/21.
-//  Modifed by Shiv Bhagat on 3/23/2021
-
+//
 import UIKit
-import SwiftUICharts
-import SwiftUI
 
-struct GraphController: View {
-    var body: some View{
-        VStack{
-            Spacer()
-            // Line chart
-            LineChartView(data: [12,22,6,1,2,7,18], title: "Test LineChart")
-            Spacer()
-             // Bar Chart
-            BarChartView(data: ChartData(values:[("Jan", 12) , ("Feb", 2) , ("Mar", 3)]
-            ) , title: "Test Bar Graph")
-        }
+class GraphController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
     }
+    
 }
-
-struct GraphController_previews: PreviewProvider {
-    static var previews: some View {
-        GraphController()
-    }
-}
-
