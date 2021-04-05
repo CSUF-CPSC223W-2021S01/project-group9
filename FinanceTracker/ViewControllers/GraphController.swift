2f2b7b3d52672013170ce9cb7d696c472d34da29
sbhagat101 <54718068+sbhagat101@users.noreply.github.com>
2021-04-04 18:31:10 -0700

    Add pods to work with charts. Also created test value for the Bar graph

--- FinanceTracker/ViewControllers/GraphController.swift
+++ FinanceTracker/ViewControllers/GraphController.swift
@@ -3,14 +3,37 @@
 //  FinanceTracker
 //
 //  Created by Justin Lee on 3/22/21.
-//
+//  modified by Shiv Bhagat on 4/03/2021
+import Charts
 import UIKit
 
-class GraphController: UIViewController {
-
+class GraphController: UIViewController, ChartViewDelegate {
+    var barChart = BarChartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        barChart.delegate = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        barChart.center = view.center
+        
+        view.addSubview(barChart)
+        
+        var entries = [BarChartDataEntry]()
+        
+        for x in 0..<10 {
+            entries.append(BarChartDataEntry(x: Double(x), y: Double(x)))
+        }
+        let set = BarChartDataSet(entries: entries, label: "Test Bar Graph")
+        
+        set.colors = ChartColorTemplates.joyful()
+        
+        let data = BarChartData(dataSet: set)
+        
+        barChart.data = data
+    }
 }
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FinanceTracker' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'Charts'
+
+  target 'FinanceTrackerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FinanceTrackerUITests' do
+    # Pods for testing
+  end
+
+end
