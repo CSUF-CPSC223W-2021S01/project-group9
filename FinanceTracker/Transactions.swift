6113ec4ac77cc21d2647b21a024f72338d78c4b0
Arm627 <monted97@gmail.com>
2021-04-08 10:34:05 -0700

    Storing accessToken on login success, and using stored access token to getTransactions

--- FinanceTracker/Transactions.swift
+++ FinanceTracker/Transactions.swift
@@ -15,33 +15,47 @@ class getTransactions {
     let transactionUrl = "https://sandbox.plaid.com/transactions/get"
     let clientId = "602e9d8caaddb00010348259"
     let secret = "d4bcd044d1751fba527799f724b043"
-    
-    //TODO: make these dynamic to the device (ie, store this id)
-    let tempAccessToken = "access-sandbox-0e3f2b1f-a589-43ef-b324-8243c55b64a4"
+
+    var accessToken = ""
     
     let startDate = "2021-03-1"
     let endDate = "2021-03-03"
     
+    init() {
+        let propertListDecoder = PropertyListDecoder()
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let archiveUrl = directory.appendingPathComponent("token").appendingPathExtension("plist")
+    
+        if let retrievedTransactions = try? Data(contentsOf: archiveUrl),
+           let decodedTransactions = try? propertListDecoder.decode(AccessToken.self, from: retrievedTransactions) {
+            self.accessToken = decodedTransactions.accessToken
+        }
+    }
+    
     func requestData(completion: @escaping ([String: Any]) -> ()) {
-        
-        let url = URL(string: self.transactionUrl)
-        let payload = "{\"client_id\":\"\(self.clientId)\",\"secret\":\"\(self.secret)\",\"access_token\":\"\(self.tempAccessToken)\", \"start_date\":\"2018-01-01\", \"end_date\":\"2021-03-01\"}".data(using: .utf8)
-        
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = payload
+        if self.accessToken != "" {
+            let url = URL(string: self.transactionUrl)
+            let payload = "{\"client_id\":\"\(self.clientId)\",\"secret\":\"\(self.secret)\",\"access_token\":\"\(self.accessToken)\", \"start_date\":\"2018-01-01\", \"end_date\":\"2021-03-01\"}".data(using: .utf8)
+            
+            var request = URLRequest(url: url!)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = payload
 
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
 
-            guard let data = data else {
-                print("error")
-                return
-            }
-            if let str = String(data: data, encoding: .utf8) {
-                return completion(self.convertToDictionary(text: str) ?? [String: Any]())
-            }
-        }.resume()
+                guard let data = data else {
+                    print("error")
+                    return
+                }
+                if let str = String(data: data, encoding: .utf8) {
+                    return completion(self.convertToDictionary(text: str) ?? [String: Any]())
+                }
+            }.resume()
+        }
+        else {
+            print("EROR")
+        }
     }
     
     func convertToDictionary(text: String) -> [String: Any]? {
