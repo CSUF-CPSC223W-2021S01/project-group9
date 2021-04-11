cfc59ca65ffacd01094ef24fc7a5501ef6d15e80
sbhagat101 <54718068+sbhagat101@users.noreply.github.com>
2021-04-10 18:55:22 -0700

    Bar chart showing transactions per month is complete

--- FinanceTracker/ViewControllers/GraphController.swift
+++ FinanceTracker/ViewControllers/GraphController.swift
@@ -17,7 +17,7 @@ class GraphController: UIViewController, ChartViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         barChart.delegate = self
-        displayLbl.text = "Test Bar Graph"
+        displayLbl.text = "Transcations Per Month"
     }
     
     override func viewDidLayoutSubviews() {
@@ -29,15 +29,73 @@ class GraphController: UIViewController, ChartViewDelegate {
         view.addSubview(barChart)
         // bar chart data values
         self.getData()
-        //var totalTranscations: Int = data.total()
-        var entries = [BarChartDataEntry]()
-        
-        for x in 0..<10 {
-            entries.append(BarChartDataEntry(x: Double(x), y: Double(x)))
+        var jan = 0, feb = 0, mar = 0, apr = 0, may = 0, jun = 0, jul = 0, aug = 0, sep = 0, oct = 0, nov = 0, dec = 0
+        let totalTranscations: Int = dataContainer.total()
+        for x in 0...(totalTranscations - 1) {
+           var transcation = dataContainer.getTransaction(index: x)
+            let date = transcation.date
+            var temp1: Character = date[date.index(date.startIndex, offsetBy: 5)]
+            var temp2: Character = date[date.index(date.startIndex, offsetBy: 6)]
+            
+            if (temp1 == "0" && temp2 == "1"){
+                jan += 1
+            }
+            else if (temp1 == "0" && temp2 == "2"){
+                feb += 1
+            }
+            else if (temp1 == "0"  && temp2 == "3"){
+                mar += 1
+            }
+            else if (temp1 == "0" && temp2 == "4"){
+                apr += 1
+            }
+            else if (temp1 == "0" && temp2 == "5"){
+                may += 1
+            }
+            else if (temp1 == "0" && temp2 == "6"){
+                jun += 1
+            }
+            else if (temp1 == "0" && temp2 == "7"){
+                jul += 1
+            }
+            else if (temp1 == "0" && temp2 == "8"){
+                aug += 1
+            }
+            else if (temp1 == "0" && temp2 == "9"){
+                sep += 1
+            }
+            else if (temp1 == "1" && temp2 == "0"){
+                oct += 1
+            }
+            else if (temp1 == "1" && temp2 == "1"){
+                nov += 1
+            }
+            else if (temp1 == "1" && temp2 == "2"){
+                dec += 1
+            }
+            else{
+                jan = 0; feb = 0; mar = 0; apr = 0; may = 0; jun = 0; jul = 0; aug = 0; sep = 0; oct = 0; nov = 0; dec = 0;
+            }
         }
-        let set = BarChartDataSet(entries: entries, label: "Test Bar Graph")
         
-        set.colors = ChartColorTemplates.joyful()
+        var entries = [BarChartDataEntry]()
+        entries.append(BarChartDataEntry(x: Double(1), y: Double(jan)))
+        entries.append(BarChartDataEntry(x: Double(2), y: Double(feb)))
+        entries.append(BarChartDataEntry(x: Double(3), y: Double(mar)))
+        entries.append(BarChartDataEntry(x: Double(4), y: Double(apr)))
+        entries.append(BarChartDataEntry(x: Double(5), y: Double(may)))
+        entries.append(BarChartDataEntry(x: Double(6), y: Double(jun)))
+        entries.append(BarChartDataEntry(x: Double(7), y: Double(jul)))
+        entries.append(BarChartDataEntry(x: Double(8), y: Double(aug)))
+        entries.append(BarChartDataEntry(x: Double(9), y: Double(sep)))
+        entries.append(BarChartDataEntry(x: Double(10), y: Double(oct)))
+        entries.append(BarChartDataEntry(x: Double(11), y: Double(nov)))
+        entries.append(BarChartDataEntry(x: Double(12), y: Double(dec)))
+        
+        
+        let set = BarChartDataSet(entries: entries, label: "Transcations per month")
+        
+        set.colors = ChartColorTemplates.pastel()
         
         let data = BarChartData(dataSet: set)
         
