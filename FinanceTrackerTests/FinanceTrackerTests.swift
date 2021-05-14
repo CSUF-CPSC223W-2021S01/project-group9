72e2975df21997f2d3e9079e2743342651baf1a9
Justin Lee <jlviper44@icloud.com>
2021-05-13 18:31:40 -0700

    Added XCTests

--- FinanceTrackerTests/FinanceTrackerTests.swift
+++ FinanceTrackerTests/FinanceTrackerTests.swift
@@ -29,5 +29,43 @@ class FinanceTrackerTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    func testStructTranactionInit() throws{
+        let test = transaction(name:"DogeCoin", amount: 200, currency: "USD", date: "05/01/21")
+        XCTAssertEqual(test.name    , "DogeCoin", "name     does not match init value")
+        XCTAssertEqual(test.amount  , 200       , "amount   does not match init value")
+        XCTAssertEqual(test.currency, "USD"     , "currency does not match init value")
+        XCTAssertEqual(test.date    , "05/01/21", "date     does not match init value")
+    }
+    func testClassDataInit() throws{
+        let dataContainer = data()
+        XCTAssertEqual(dataContainer.total(), 0, "Data class must be initialized with 0 transactions")
+    }
+    func testClassDataAdd() throws{
+        let dataContainer = data()
+        let transactionTest = transaction(name:"DogeCoin", amount: 200, currency: "USD", date: "05/01/21")
+        dataContainer.addTransaction(transaction:transactionTest)
+        
+        let test = dataContainer.getTransaction(index: 0)
+        XCTAssertEqual(test.name    , "DogeCoin", "name     does not match init value")
+        XCTAssertEqual(test.amount  , 200       , "amount   does not match init value")
+        XCTAssertEqual(test.currency, "USD"     , "currency does not match init value")
+        XCTAssertEqual(test.date    , "05/01/21", "date     does not match init value")
+    }
+    func testClassDataClear() throws{
+        let dataContainer = data()
+        let transactionTest = transaction(name:"DogeCoin", amount: 200, currency: "USD", date: "05/01/21")
+        dataContainer.addTransaction(transaction:transactionTest)
+        
+        dataContainer.clear()
+        XCTAssertEqual(dataContainer.total(), 0, "Data class must have 0 transactions when using clear")
+    }
+    func testClassDataTotal() throws{
+        let dataContainer = data()
+        let transactionTest = transaction(name:"DogeCoin", amount: 200, currency: "USD", date: "05/01/21")
+        dataContainer.addTransaction(transaction:transactionTest)
+        dataContainer.addTransaction(transaction:transactionTest)
+        dataContainer.addTransaction(transaction:transactionTest)
+        
+        XCTAssertEqual(dataContainer.total(), 3, "Data class does not have correct total")
+    }
 }
